c6f0b998e614d6477937f3752e187501fa36e9b4
Elżbieta Obara <elzbieta.obara@kruksa.pl>
2025-10-02 18:08:20 +0200

    kolejne linki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,38 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   items = [
+
+ { 
+      title: 'Lewin Kłodzki rekolekcje (różne lata)', 
+      show: false,
+      links: [
+        { name: 'Film 1', url:'https://drive.google.com/file/d/1dljUMxwhUrTb6Lf3_0UkFojdKVJB5nnC/view?usp=sharing'},
+        { name: 'Album zdjęć', url: 'https://drive.google.com/file/d/1dljUMxwhUrTb6Lf3_0UkFojdKVJB5nnC/view?usp=sharing' }
+      ]
+    },
+
+    { 
+      title: '2025-06-19 Wniebogłosy', 
+      show: false,
+      links: [
+        { name: 'Film 1', url:'https://drive.google.com/file/d/1RD5D9Op3MbeZHNNukB8idsmLiLOQyHix/view?usp=sharing'},
+        { name: 'Film 2', url:'https://drive.google.com/file/d/12ed5-y2IB609eF10kC8QIikIneaFKm5D/view?usp=sharing'},
+        { name: 'Film 3', url:'https://drive.google.com/file/d/1IQ1DxYKIxOnSPBYAcoLFEO3dtcW293oY/view?usp=sharing'},
+        { name: 'Film 4', url:'https://drive.google.com/file/d/10FB2rRfoxmlgvjLADFm4vkBMjNDWb_N1/view?usp=sharing'},
+        { name: 'Film 5', url:'https://drive.google.com/file/d/1oOK9a4pgeOTyLqM52PQ-rDM3AfblLkKK/view?usp=sharing'},
+        { name: 'Film 6', url:'https://drive.google.com/file/d/1wsr0UBsdG0uAOJfKUsBw-EcO_1CbnsmK/view?usp=sharing'},
+        { name: 'Film 7', url:'https://drive.google.com/file/d/1V7768UmItqDbk0GujosgnNTc8uFXKyAS/view?usp=sharing'},
+        { name: 'Film 8', url:'https://drive.google.com/file/d/1be5iGHRCsVLNFHg5qcM_LwPbdP6cjrhK/view?usp=sharing'},
+        { name: 'Film 9', url:'https://drive.google.com/file/d/12L3wt-3LuQgqtUdFvPGowG5zPhnsLH-4/view?usp=sharing'},
+        { name: 'Film 10', url:'https://drive.google.com/file/d/1va4-sahc3p81-V2xyXQellN2aiwYFS-S/view?usp=sharing'},
+        { name: 'Film 11', url:'https://drive.google.com/file/d/1NMS7qxHVu4U9EtsHspy9p5keuDP_uWil/view?usp=sharing'},
+        { name: 'Film 12', url:'https://drive.google.com/file/d/16duXGjCWEiCZLbQplpYEuo_rqLaEH1zh/view?usp=sharing'},
+        { name: 'Film 13', url:'https://drive.google.com/file/d/18326TYDRwaPQcHhV_CT3HNk_GJNtJuW9/view?usp=sharing'},
+        { name: 'Film 14', url:'https://drive.google.com/file/d/1IrKI5GpZjpktCh9FExkN2TF83WRNaY4O/view?usp=sharing'},
+        { name: 'Film 15', url:'https://drive.google.com/file/d/1U4l6CEO-K8Uj4LhM3n83tLIaG40ZoQGz/view?usp=sharing'},
+        { name: 'Album zdjęć', url: 'https://photos.app.goo.gl/mS69ooJixbNriKseA' }
+      ]
+    },
     { 
       title: '2025 Częstochowa rekolekcje', 
       show: false,
